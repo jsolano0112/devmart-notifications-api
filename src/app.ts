97fee2397bb9c767a1892760daca4f5882bcf70c
juanasolano <juana.solano@vensure.com>
2026-05-18 08:49:26 -0500

    update origin

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: "http://localhost:5173", //Front
+  origin: "http://localhost:5173,http://localhost:4000", //Front
    credentials: false
 }));
 
